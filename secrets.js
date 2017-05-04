b61d0edf4334a7d2fe534df647d50d22e5defa70
Edward Goo <Edwardx89@yahoo.com>
2017-05-04 12:39:01 -0400

    secrets

--- secrets.js
+++ secrets.js
@@ -1,7 +1,7 @@
 
 module.exports = {
   apiAi: 'e9923778617741dcaddf488f97e76a0f',
-  facebook: 'EAAJNV0gwu48BADD8xqZCZBLFO03kJ0zOhustzTJ25Syr76CHkracgLLi08YaizaTYSBNm3o48N3ZAi7ATZB5ETA4Ge6sPkMMTTxGGJTs8iyO2UuomlZCZBepxflWkJ9pzvsHpG2jwpWLn5bMDF1l8bwnGyhEVELSpWQrnfIi8fJAZDZD',
+  facebook: 'EAAJNV0gwu48BAJYcgQFiWb3t7rwgtG0ansUNvuBEALFVm8XXosvS99HzqdVCA2WMjZB5Yxn3fTmXErT11mZAqR7qBAeVYCggVA3hM3gKVsamBuakZCWohboKSugAZCtlV5bAoiq6UiIpa5ZBBDU0zw7zgyoshFUKVewwjzNR1gwZDZD',
   weather: '61b2990a53fb41849e3133951172804',
   yelpClientId: '1XqO594cIJbfiwWnqByNYQ',
   yelpClientSecret: 'aYex6oAFImpwyBOj8vrlV8tu7BCC86oSzjRgserAKtx7PEYssaX7PP5NVwwboTMf',
